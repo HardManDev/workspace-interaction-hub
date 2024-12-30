571b0b3fd95931c43e73b469ea62c1d821db1d78
HardManDev <hardman.develop@proton.me>
2024-12-30 17:50:18 +0300

    feat: add PostgreSQL with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,21 @@
+import * as process from 'node:process'
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-    imports: []
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DATABASE_HOST,
+            port: Number(process.env.DATABASE_PORT),
+            username: process.env.DATABASE_USERNAME,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
+            autoLoadEntities: true,
+            synchronize: process.env.NODE_ENV === 'development'
+        })
+    ]
 })
 export class AppModule {}
